72eb993aea131f9eb617bc6d69ab5183e9b59312
rfhaya <146700597+rfhaya@users.noreply.github.com>
2023-10-04 00:10:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any}', function () {
-    return view('app'); 
-})->where('any', '.*');
-
-Route::get('/listArticle', function (Request $request) {
-    $article = Article::paginate(10);
-    return $article;
+Route::get('/', function () {
+    return view('welcome');
 });
